08c83f7d55b14a20b349cfd0f6ce4a801ca8422a
estudiante <estudiante@ISIS2603-400.sis.virtual.uniandes.edu.co>
2019-02-11 11:49:24 -0500

    Listo Juan David Diaz

--- s1_eventos-api/src/main/java/co/edu/uniandes/csw/eventos/dtos/CalificacionDTO.java
+++ s1_eventos-api/src/main/java/co/edu/uniandes/csw/eventos/dtos/CalificacionDTO.java
@@ -7,38 +7,63 @@ package co.edu.uniandes.csw.eventos.dtos;
 
 /**
  *
- * @author estudiante
+ * @author Juan David Díaz 
  */
 public class CalificacionDTO {
+    /*
+        Atributo de tipo int que indica la cantidad de estrellas de la calificacion
+    */
+   
     private int estrellas;
+    /*
+        Atributo de  tipo String que contiene el comentario de la calificacion
+    */
     private String comentario;
+    /*
+        Atributo de tipo booleano que indica si se recomienda o no
+    */
     private boolean recomendado;
 
+    /*
+        Constructor
+    */
       public EntradaDTO()
     {
         
     }
-      
+      /**
+       * @return estrellas.  Retorna las estrellas
+      **/
     public int getEstrellas() {
         return estrellas;
     }
-
+     /**
+       * @return comentario. Retorna el comentario
+      **/
     public String getComentario() {
         return comentario;
     }
-
+    /**
+       * @return recomendado. Retorna si es recomendado
+      **/
     public boolean isRecomendado() {
         return recomendado;
     }
-
+    /**
+       @param estrellas. Reinicializa las estrellas
+      **/
     public void setEstrellas(int estrellas) {
         this.estrellas = estrellas;
     }
-
+    /**
+       * @param comentario. Reinicializa el comentario
+      **/
     public void setComentario(String comentario) {
         this.comentario = comentario;
     }
-
+    /**
+       * @param recomendado. Reinicializa si es recomendado
+      **/
     public void setRecomendado(boolean recomendado) {
         this.recomendado = recomendado;
     }
--- s1_eventos-api/src/main/java/co/edu/uniandes/csw/eventos/dtos/EntradaDTO.java
+++ s1_eventos-api/src/main/java/co/edu/uniandes/csw/eventos/dtos/EntradaDTO.java
@@ -13,80 +13,127 @@ import java.io.Serializable;
  */
 public class EntradaDTO implements Serializable {
     
+    /*
+        Atributo de un tipo por definir que almacena el codigo QR
+    */
     private undefined QR;
+    /*
+        Atributo de tipo string que contiene la descripcion de la entrada
+    */
     private String descripcion;
+    /*
+        Atributo de tipo double que representa el precio de la entrada
+    */
     private Double precio ;
+    /*
+    Atributo de tipo String que representa la locacion de la entrada
+    */
     private String locacion;
+    /*
+        Atributo de tipo int que indica el numero de la boleta
+    */
     private int numero;
+    /*
+        Atributo de tipo booleano que indica si la boleta esta disponible
+    */
     private boolean disponible;
+    /*
+        Atributo de tipo booleano que indica si ya se hizo check-In con la boleta
+    */
     private boolean checkIn;
+    /*
+        Atributo de tipo booleano que indica si la boleta esta reservada
+    */
     private boolean reservado;
 
+    /*
+        Constructor 
+    */
     public EntradaDTO()
     {
         
     }
-    
+    /**
+        @return QR. Retorna el QR
+    **/
     public undefined getQR() {
         return QR;
     }
-
+    /**
+        @return descripcion. Retorna la descripcion
+    **/
     public String getDescripcion() {
         return descripcion;
     }
-
+    /**
+     *  @return precio. Retorna el precio
+    **/
     public Double getPrecio() {
         return precio;
     }
-
+    /**
+        @return locacion.   Retorna la locacion
+    **/
     public String getLocacion() {
         return locacion;
     }
-
+    /**
+     * @return numero. Retorna el numero de la entrada
+    **/
     public int getNumero() {
         return numero;
     }
-
+    /**
+     *  @return disponible. Retorna si esta disponible, true, de lo contrario false
+    **/
     public boolean isDisponible() {
         return disponible;
     }
-
+    /**
+       * @reteurn checkIn. Retorna si ya se realizo checkIn, true, de lo contrario false
+    **/
     public boolean isCheckIn() {
         return checkIn;
     }
-
+    /**
+     *  @return reservado. Retorna si esta reservada, true, de lo contrario false
+    **/
     public boolean isReservado() {
         return reservado;
     }
-
+    /**
+     * @param QR.  Reinicializa el valor del QR
+    **/
     public void setQR(undefined QR) {
         this.QR = QR;
     }
-
+    /**
+     * @param descripcion. Reinicializa el valor de la descripcion
+    **/
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-
+    /**
+     *@param precio. Reinicializa el valor del precio
+    **/
     public void setPrecio(Double precio) {
         this.precio = precio;
     }
-
-    public void setLocacion(String locacion) {
-        this.locacion = locacion;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
+    /**
+     * @param disponible. Reinicializa el valor de disponible
+    **/
     public void setDisponible(boolean disponible) {
         this.disponible = disponible;
     }
-
+    /**
+      * @param checkIn  Reinicializa el valor de checkIn
+    **/
     public void setCheckIn(boolean checkIn) {
         this.checkIn = checkIn;
     }
-
+    /**
+      *@param reservado  Reinicializa el valor de reservado
+    **/
     public void setReservado(boolean reservado) {
         this.reservado = reservado;
     }
