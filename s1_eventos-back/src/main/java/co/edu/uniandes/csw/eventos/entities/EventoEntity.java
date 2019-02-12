9530ede731a0e5a7ba90ffff07fe361aaeb8920d
Mateo <m.vallejom1@uniandes.edu.co>
2019-02-11 23:04:41 -0500

    Evento entity y persistence

---
+++ s1_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/entities/EventoEntity.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author estudiante
+ */
+@Entity
+public class EventoEntity extends BaseEntity implements Serializable {
+    
+     public  enum Categoria{
+ 
+}
+    //Atributos
+    
+    private String nombre;
+    private String descripcion;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private String detalles;
+    private Categoria categoria;
+    private boolean privado;
+    private int capacidadMaxima;
+    private int boletasDisponibles;
+    private long id;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * @return the detalles
+     */
+    public String getDetalles() {
+        return detalles;
+    }
+
+    /**
+     * @param detalles the detalles to set
+     */
+    public void setDetalles(String detalles) {
+        this.detalles = detalles;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the privado
+     */
+    public boolean isPrivado() {
+        return privado;
+    }
+
+    /**
+     * @param privado the privado to set
+     */
+    public void setPrivado(boolean privado) {
+        this.privado = privado;
+    }
+
+    /**
+     * @return the capacidadMaxima
+     */
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    /**
+     * @param capacidadMaxima the capacidadMaxima to set
+     */
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    /**
+     * @return the boletasDisponibles
+     */
+    public int getBoletasDisponibles() {
+        return boletasDisponibles;
+    }
+
+    /**
+     * @param boletasDisponibles the boletasDisponibles to set
+     */
+    public void setBoletasDisponibles(int boletasDisponibles) {
+        this.boletasDisponibles = boletasDisponibles;
+    }
+}
