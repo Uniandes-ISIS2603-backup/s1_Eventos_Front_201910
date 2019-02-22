0d924cecaf720e8af5e4f9188c86b7a18a76aa30
Juan David Diaz <jd.diazc@uniandes.edu.co>
2019-02-21 20:42:28 -0500

    Adelantado algunas clases. Falta implementar correctamente los recursos

--- s1_eventos-api/src/main/java/co/edu/uniandes/csw/eventos/resources/CalificacionResource.java
+++ s1_eventos-api/src/main/java/co/edu/uniandes/csw/eventos/resources/CalificacionResource.java
@@ -6,10 +6,16 @@
 package co.edu.uniandes.csw.eventos.resources;
 
 import co.edu.uniandes.csw.eventos.dtos.CalificacionDTO;
+import co.edu.uniandes.csw.eventos.dtos.CalificacionDetailDTO;
 import co.edu.uniandes.csw.eventos.dtos.EventoDTO;
+import co.edu.uniandes.csw.eventos.ejb.CalificacionLogic;
+import co.edu.uniandes.csw.eventos.entities.CalificacionEntity;
+import co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -18,13 +24,14 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
  * @author Juan David Diaz
  */
 
-@Path("calificacion")
+@Path("calificaciones")
 @Produces("application/json")
 @Consumes("application/json")
 @RequestScoped
@@ -32,33 +39,56 @@ public class CalificacionResource {
     
     private static final Logger LOGGER = Logger.getLogger(CalificacionResource.class.getName());
     
+    @Inject
+    private CalificacionLogic calificacionLogic;
+    
+    //@Inject
+    //private UsuarioLogic usuarioLogic;
     
    @POST
-   public CalificacionDTO createCalificacion(CalificacionDTO calificacion)
+   public CalificacionDTO createCalificacion(CalificacionDTO calificacion) throws BusinessLogicException
    {
-   return calificacion;
+       CalificacionDTO calificacionDTO = new CalificacionDTO(calificacionLogic.createCalificacion(calificacion.toEntity()));
+        return calificacion;
    }
    
     @GET 
-   public List<CalificacionDTO> getCalificacion(){
-       return null;
+   public List<CalificacionDetailDTO> getCalificaciones(){
+       List<CalificacionDetailDTO> listaCalificaciones = listEntity2DetailDTO(calificacionLogic.findAll());
+       return listaCalificaciones;
    }
    
    @GET
    @Path("{calificacionId: \\d+}")
-   public CalificacionDTO getCalificacion(@PathParam("calificacionId") Long calificacionId){
-       return null;
+   public CalificacionDetailDTO getCalificacion(@PathParam("calificacionesId") Long calificacionId){
+       CalificacionEntity calificacionEntity = calificacionLogic.findCalificacion(calificacionId);
+       if(calificacionEntity == null)
+       {
+           throw new WebApplicationException("El recurso /calificaciones/" + calificacionId + " no existe.", 404);
+       }
+       CalificacionDetailDTO calificacionDetailDTO = new CalificacionDetailDTO(calificacionEntity);
+       return calificacionDetailDTO;
    }
    
    @PUT
    @Path("(calificacionId: \\d+")
-   public CalificacionDTO updateCalificacion (@PathParam("calificacionId") Long calificacionId, CalificacionDTO calificacion){
+   public CalificacionDTO updateCalificacion (@PathParam("calificacionesId") Long calificacionId, CalificacionDTO calificacion){
+       
        return calificacion;
    }
    
    @DELETE
    @Path("(calificacionId: \\d+)")
-   public void deleteCalificacion (@PathParam("calificacionId") Long calificacionId){
+   public void deleteCalificacion (@PathParam("calificacionesId") Long calificacionId){
        
+   }
+   
+   private List<CalificacionDetailDTO> listEntity2DetailDTO(List<CalificacionEntity> entityList)
+   {
+       List<CalificacionDetailDTO> list = new ArrayList<>();
+       for(CalificacionEntity entity : entityList){
+           list.add(new CalificacionDetailDTO(entity));
+       }
+       return list;
    }
 }
---
+++ s1_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/persistence/CalificacionPersistenceTest.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.test.persistence;
+
+import co.edu.uniandes.csw.eventos.entities.CalificacionEntity;
+import javax.inject.Inject;
+import org.junit.runner.RunWith;
+import org.jboss.arquillian.junit.Arquillian;
+import co.edu.uniandes.csw.eventos.persistence.CalificacionPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+import org.junit.Assert;
+
+/**
+ *
+ * @author Juan David Diaz
+ */
+@RunWith(Arquillian.class)
+public class CalificacionPersistenceTest {
+    
+    @Inject
+    private CalificacionPersistence calificacionPersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    @Inject
+    UserTransaction utx;
+    
+    private List<CalificacionEntity> data = new ArrayList<CalificacionEntity>();
+    
+    @Deployment
+    public static JavaArchive createDeployment()
+    {
+        return ShrinkWrap.create(JavaArchive.class).addPackage(CalificacionEntity.class.getPackage())
+                .addPackage(CalificacionEntity.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml","persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from CalificacionEntity").executeUpdate();
+    }
+    
+     private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+
+            CalificacionEntity entity = factory.manufacturePojo(CalificacionEntity.class);
+
+            em.persist(entity);
+
+            data.add(entity);
+        }
+    }
+     
+     @Test
+     public void createCalificacionTest()
+     {
+          PodamFactory factory = new PodamFactoryImpl();
+          CalificacionEntity newEntity = factory.manufacturePojo(CalificacionEntity.class);
+          CalificacionEntity result = calificacionPersistence.create(newEntity);
+          
+          Assert.assertNotNull(result);
+          
+          CalificacionEntity entity = em.find(CalificacionEntity.class,result.getId());
+          
+          Assert.assertEquals(newEntity.getId(),entity.getId());
+     }
+}
