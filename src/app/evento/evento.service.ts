804312649cc0d838b2072d22463b69aa92d8ddaa
Paula Molina <pa.molina@uniandes.edu.co>
2019-05-14 09:51:14 -0500

    Vuelve Borrar evento

--- src/app/evento/evento.service.ts
+++ src/app/evento/evento.service.ts
@@ -8,7 +8,6 @@ import {EventoDetail}from './evento-detail';
 
 
 import { environment } from '../../environments/environment';
-import { Calificacion } from '../calificacion/calificacion';
 const API_URL = environment.apiURL;
 const eventos = '/eventos';
 
@@ -63,5 +62,13 @@ export class EventoService {
    getEventoDetail(evento_id): Observable<EventoDetail> {
     return this.http.get<EventoDetail>(API_URL + eventos + '/' + evento_id);
     }
-
+    
+    /**
+    * Elimina un evento
+    * @param eventoId The editorial which will be deleted
+    * @returns True if the editorial was deleted, false otherwise
+    */
+    deleteEvento(eventoId): Observable<EventoDetail> {
+        return this.http.delete<EventoDetail>(API_URL + eventos + '/' + eventoId);
+    }
 }
